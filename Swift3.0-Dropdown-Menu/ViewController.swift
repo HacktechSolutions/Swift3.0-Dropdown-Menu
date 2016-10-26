a97882aa2db5dc9fdfc7170b86a2ea35b661586a
biodecity <Marcos@MacBook-Pro-de-Marcos.local>
2016-10-26 13:29:44 -0200

    adding xcodeProject

--- Swift3.0-Dropdown-Menu/ViewController.swift
+++ Swift3.0-Dropdown-Menu/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var drop: DropMenuButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        drop.initMenu(["Item A", "Item B", "Item C"], actions: [({ () -> (Void) in
+            print("Estou fazendo a ação A")
+        }), ({ () -> (Void) in
+            print("Estou fazendo a ação B")
+        }), ({ () -> (Void) in
+            print("Estou fazendo a ação C")
+        })])
     }
 
     override func didReceiveMemoryWarning() {
